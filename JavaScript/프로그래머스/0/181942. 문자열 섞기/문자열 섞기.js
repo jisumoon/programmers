356e936df3ddd4ec0836863635ce7c1955370aa6
jisumoon <jjisu97@naver.com>
2024-09-26 00:25:46 +0900

    [level 0] Title: 문자열 섞기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -0,0 +1,11 @@
+const solution = (str1, str2) => {
+    let answer = '';
+    
+    for (let i = 0; i < str1.length; i++) {
+        answer += str1[i];  
+        answer += str2[i];  
+    }
+    
+    return answer;  
+};
+
