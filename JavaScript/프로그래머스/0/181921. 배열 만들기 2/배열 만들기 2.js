56ce4df81daf03c74b2bda59fc912edbf92762b5
jisumoon <jjisu97@naver.com>
2024-12-26 21:03:01 +0900

    [level 0] Title: 배열 만들기 2, Time: 120.98 ms, Memory: 52.8 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -0,0 +1,12 @@
+const solution = (l, r) => {
+    const result = [];
+
+    for (let i = l; i <= r; i++) {
+        const str = i.toString(); 
+        if ([...str].every(ch => ch === '0' || ch === '5')) {
+            result.push(i); 
+        }
+    }
+
+    return result.length > 0 ? result : [-1];
+};
