55035c6685aae26635bf42a364f2ebc1e6b78973
jisumoon <jjisu97@naver.com>
2024-12-26 20:50:28 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 53.21 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -0,0 +1,10 @@
+const solution = (arr, queries) => {
+    for (const [s, e, k] of queries) {
+        for (let i = s; i <= e; i++) {
+            if (i % k === 0) { 
+                arr[i] += 1; 
+            }
+        }
+    }
+    return arr; 
+};
