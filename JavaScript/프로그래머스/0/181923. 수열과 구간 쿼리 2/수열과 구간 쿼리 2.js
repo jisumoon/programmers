c469f383fe23be870d65e9d83c899c499823357b
jisumoon <jjisu97@naver.com>
2024-12-26 20:41:49 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 37.32 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -0,0 +1,18 @@
+const solution = (arr, queries) => {
+    const result = []; 
+
+    for (const [s, e, k] of queries) {
+        let min = Infinity; 
+
+        for (let i = s; i <= e; i++) {
+            if (arr[i] > k && arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+
+        result.push(min === Infinity ? -1 : min);
+    }
+
+    return result;
+};
